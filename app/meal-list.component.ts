b8d5f2b4c0234569684476bced7faf2f861b597d
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-09-15 07:40:28 +0300

    Create Meal List Component Create Meal List Component for displaying a list of meals logged by the user

---
+++ app/meal-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Meal } from './meal.model';
+
+@Component({
+    selector : 'meal-list',
+    template : `
+       <div *ngFor="let currentMeal of childMealList">
+          <h4> {{currentMeal.name}} </h4>
+          <p> {{currentMeal.details}} <p>
+          <p>  {{currentMeal.calories}} <p>
+      </div>
+    `
+})
+
+export class MealListComponent {
+    @Input() childMealList : Meal [];
+}
