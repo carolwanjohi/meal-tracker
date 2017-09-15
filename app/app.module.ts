9f68af2a6432e80dd59813675f8d57309cc9d64c
Carol Wanjohi <wanjohi.carol54@gmail.com>
2017-09-15 06:06:23 +0300

    Add FormModule Import FormModule from angular to create form fields with ngModel

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,17 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormModule } from '@angular/forms';
 import { AppComponent }   from './app.component';
 
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+    imports: [
+        BrowserModule,
+        FormModule,
+    ],
+    declarations: [ 
+        AppComponent, 
+    ],
+    bootstrap:    [ AppComponent ]
 })
 
 export class AppModule { }
